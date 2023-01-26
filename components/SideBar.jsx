13db8ad5924786beca72597114a814d0d813fc3c
Calathea-Z <zsykes21@gmail.com>
2023-01-26 16:41:13 -0500

    Begin work on SideBar portion.

---
+++ components/SideBar.jsx
@@ -0,0 +1,22 @@
+import { HomeIcon, SearchIcon, LibraryIcon, PlusCircleIcon } from "@heroicons/react/24/outline";
+
+const Sidebar = () => {
+
+return (
+    <>
+    <div>
+        <div>
+            <button>
+                <HomeIcon className='h-5 w-5'/>
+                <p>Home</p>
+            </button>
+        </div>
+    </div>
+
+    </>
+
+)
+}
+
+
+export default Sidebar
