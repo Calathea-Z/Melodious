b5dc1552ae2f814804d8e26f93e8448a7f2df7ad
Calathea-Z <zsykes21@gmail.com>
2023-01-30 22:50:40 -0500

    Fixed a token issue that was causing the images to not load sometime.

--- components/MainFeed.jsx
+++ components/MainFeed.jsx
@@ -28,7 +28,7 @@ function MainFeed() {
 return (
     <div className='flex-grow h-screen overflow-y-scroll scrollbar-hide'>
         <header className='absolute top-5 right-8'>
-            <div className='flex items-center bg-purp space-x-3 opacity-90 hover:opacity-80 text-white cursor-pointer rounded-full p-1 pr-2' onClick={() => signOut()}>
+            <div className='flex items-center bg-purple-800 space-x-3 opacity-90 hover:opacity-80 text-white cursor-pointer rounded-full p-1 pr-2' onClick={() => signOut()}>
                 <img src={session?.user.image} className='rounded-full w-10 h-10'/>
                 <h2>{session?.user.name}</h2>
                 <ChevronDoubleDownIcon className='w-5 h-5'/>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import { getSession } from 'next-auth/react';
 import MainFeed from '../components/MainFeed';
 import SideBar from '../components/SideBar';
 
@@ -18,5 +18,14 @@ const Home = () => {
     </div>
   )
 }
-
 export default Home
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session
+    }
+  }
+}
