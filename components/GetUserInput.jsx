a2bf6f62b27a8e21a8977866af021abb25db8d46
Calathea-Z <zsykes21@gmail.com>
2023-01-31 15:37:06 -0500

    State propery functioning for user form.

--- components/GetUserInput.jsx
+++ components/GetUserInput.jsx
@@ -1,14 +1,38 @@
+import { PuzzlePieceIcon } from "@heroicons/react/24/solid"
+import { useState } from "react"
+
 function GetUserInput() {
+    const [userInput, setUserInput] = useState(null);
+
+
+    const setInput = (e) => {
+        const input = e.target.value
+        setUserInput(input)
+    }
+
+    console.log(userInput);
+
+
+
+
   return (
-    <div className='flex-grow h-screen overflow-y-scroll scrollbar-hide'>
-        <div className='flex items-end space-x-7  h-80 text-white p-8'>
-        <h1>What Are You Feeling? WHY IS THERE NO TEXT</h1>
+    <div className='flex-grow h-screen overflow-y-scroll items-center justify-center scrollbar-hide'>
+        <div className='flex flex-col justify-center items-start space-x-7 bg-gradient-to-b to-purp from-greeen h-80 p-5 text-purple-200'>
+            <div className='mt-5'>
+                <h1 className='font-extrabold tracking-wider font-mono text-2xl'>What kind of music are you in the mood for?</h1>
+            </div>
+            <div className='flex p-20 items-center jusity-center'>
+                <form className='flex space-x-20' onChange={setInput}>
+                    <input type='text' name='songValues' className='text-black border-none rounded-sm' />
+                    <button type='submit' value='|' className='button text-purple-300 text-2xl font-extrabold'>
+                        <PuzzlePieceIcon className='button text-2xl mb-2' />
+                        <h6>Go!</h6>
+                    </button>
+                </form>
+            </div>
         </div>
-        <form className='h-20'>
-            <input type='text' name='songValues' />
-            <input type='submit' value='submit' />
-        </form>
-        </div>
+        
+    </div>
   )
 }
 
