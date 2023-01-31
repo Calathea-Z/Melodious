a639a441c0fb1c19cc0eb8372ec8de4bba7e620b
Calathea-Z <zsykes21@gmail.com>
2023-01-31 11:47:30 -0500

    Basic play paucse functionality working. Skip track not working. Not sure why. Go back track will start the song over but not skip to the previous track.

--- components/Player.jsx
+++ components/Player.jsx
@@ -4,6 +4,8 @@ import { useSession } from 'next-auth/react';
 import { useRecoilState } from 'recoil';
 import { currentTrackIdState, isPlayingState } from '../atoms/songAtom';
 import useSongInfo from '../hooks/useSongInfo';
+import { ArrowPathIcon, BackwardIcon, BeakerIcon, ForwardIcon, PauseCircleIcon, PlayCircleIcon } from '@heroicons/react/24/solid';
+import { SpeakerWaveIcon } from '@heroicons/react/24/outline';
 
 
 function Player() {
@@ -37,8 +39,21 @@ function Player() {
         }
 
     },[currentTrackId, spotifyApi, session])
+
+    const handlePlayPause = () => {
+        spotifyApi.getMyCurrentPlaybackState().then((data) => {
+            if (data.body.is_playing) {
+                spotifyApi.pause();
+                setIsPlaying(false);
+            }else{
+                spotifyApi.play();
+                setIsPlaying(true);
+            }
+        });
+    }
     
 return (
+
     <div className='h-24 bg-gradient-to-b from-greeen to-purp text-purple-300 grid grid-cols-3 text-xs md:text-base px-2 md:px-8'>
         <div className='flex items-center space-x-4'>
             <img src={songInfo?.album.images?.[0]?.url} alt='' className='hidden md:inline h-10 w-10' />
@@ -46,13 +61,19 @@ return (
                 <h3>{songInfo?.name}</h3>
                 <p>{songInfo?.artists?.[0]?.name}</p>
             </div>
-
-            <div>
-                <Swi
-
+        </div>
+        <div className='flex items-center justify-evenly'>
+                <BeakerIcon className='button' />
+                <BackwardIcon className='button' onClick={() => spotifyApi.skipToPrevious()} />
+                {isPlaying ? (
+                    <PauseCircleIcon className='button' onClick={handlePlayPause} />
+                ) : (
+                    <PlayCircleIcon className='button' onClick={handlePlayPause} />
+                )}
+                <ForwardIcon className='button' onClick={() => spotifyApi.skipToNext} />
+                <ArrowPathIcon className='button' />
             </div>
         </div>
-    </div>
 )}
 
 export default Player
