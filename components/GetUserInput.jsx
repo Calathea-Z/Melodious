a265ee1ce1bb2f6c24a2e318413a8759aafcb675
Calathea-Z <zsykes21@gmail.com>
2023-01-31 16:25:09 -0500

    Fix issue where page was reloading on every search.

--- components/GetUserInput.jsx
+++ components/GetUserInput.jsx
@@ -1,17 +1,22 @@
 import { PuzzlePieceIcon } from "@heroicons/react/24/solid"
 import { useState } from "react"
+import useSpotify from "../hooks/useSpotify";
 
 
 function GetUserInput() {
     const [userInput, setUserInput] = useState(null);
+    const spotifyApi = useSpotify();
 
 
     const setInput = (e) => {
         const input = e.target.value
         setUserInput(input)
     }
-
     console.log(userInput);
+
+    const search = async () => {
+       console.log("Search for", userInput)
+    }
 
 
 
@@ -25,7 +30,7 @@ function GetUserInput() {
             <div className='flex p-20 items-center jusity-center'>
                 <form className='flex space-x-20' onChange={setInput}>
                     <input type='text' name='songValues' className='text-black border-none rounded-sm' />
-                    <button type='submit' value='|' className='button text-purple-300 text-2xl font-extrabold'>
+                    <button type='button' value='|' className='button text-purple-300 text-2xl font-extrabold' onClick={search}>
                         <PuzzlePieceIcon className='button text-2xl mb-2' />
                         <h6>Go!</h6>
                     </button>
