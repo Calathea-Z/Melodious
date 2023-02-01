302e80c79e7a726ea5b4d87feb2a9648f40136b3
Calathea-Z <zsykes21@gmail.com>
2023-02-01 09:14:18 -0500

    Added favicon.io

--- components/GetUserInput.jsx
+++ components/GetUserInput.jsx
@@ -5,6 +5,7 @@ import useSpotify from "../hooks/useSpotify";
 
 function GetUserInput() {
     const [userInput, setUserInput] = useState(null);
+    const [songList, setSongList] = useState([]);
     const spotifyApi = useSpotify();
 
 
@@ -16,7 +17,19 @@ function GetUserInput() {
 
     const search = async () => {
        console.log("Search for", userInput)
+
+    const artistID = spotifyApi.searchArtists(userInput).then((data) => { console.log(data)
+        return data.items[0].id;
+    })
+    .catch((err) => console.log("Yikes", err));
+     console.log("This is ArtistIDartistID", artistID)
+
     }
+
+   
+
+    //Need to first grab artist ID
+    //Grab top songs for that artists ID
 
 
 
@@ -25,7 +38,7 @@ function GetUserInput() {
     <div className='flex-grow h-screen overflow-y-scroll items-center justify-center scrollbar-hide'>
         <div className='flex flex-col justify-center items-start space-x-7 bg-gradient-to-b to-purp from-greeen h-80 p-5 text-purple-200'>
             <div className='mt-5'>
-                <h1 className='font-extrabold tracking-wider font-mono text-2xl'>What kind of music are you in the mood for?</h1>
+                <h1 className='font-extrabold tracking-wider font-mono text-2xl'>Search by Artist?</h1>
             </div>
             <div className='flex p-20 items-center jusity-center'>
                 <form className='flex space-x-20' onChange={setInput}>
