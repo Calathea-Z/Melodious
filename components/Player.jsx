cbb8e31eb3a4b5c96feb06aad72098b99303098e
Calathea-Z <zsykes21@gmail.com>
2023-02-05 16:57:03 -0500

    Fixing mobile styling on mobile. Adding in volume slider.

--- components/Player.jsx
+++ components/Player.jsx
@@ -1,7 +1,8 @@
 import DropUpPlaylistSelector from "./DropUpPlaylistSelector";
 import useSongInfo from "../hooks/useSongInfo";
 import useSpotify from "../hooks/useSpotify";
-import { BackwardIcon, PauseCircleIcon, PlayCircleIcon, ForwardIcon } from "@heroicons/react/24/outline";
+import { BackwardIcon, PauseCircleIcon, PlayCircleIcon, ForwardIcon, SpeakerXMarkIcon } from "@heroicons/react/24/outline";
+import { SpeakerWaveIcon } from "@heroicons/react/24/solid";
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
@@ -54,14 +55,14 @@ function Player() {
 
   
   return (
-    <div className="h-24 bg-gradient-to-b from-gray-800 to-gray-900 text-yellow-400 flex justify-between md:grid md:grid-cols-3 text-xs md:text-base px-2 md:px-8">
+    <div className="h-24 bg-gradient-to-b from-gray-800 to-gray-900 text-yellow-400 grid grid-cols-3 text-xs md:text-base px-2 space-x-2 md:px-8">
       <div className="flex items-center space-x-4">
         <img
           src={songInfo?.album.images?.[0]?.url}
           alt=""
-          className="hidden md:inline h-10 w-10"
+          className=" hidden md:inline h-10 w-10"
         />
-        <div>
+        <div className="justify-self-start">
           <h3>{songInfo?.name}</h3>
           <p>{songInfo?.artists?.[0]?.name}</p>
         </div>
@@ -78,8 +79,17 @@ function Player() {
         )}
         <ForwardIcon className="button" onClick={() => spotifyApi.skipToNext} />
       </div>
-      <div className="items-center align-middle flex md:hidden">
-        <DropUpPlaylistSelector />
+      <div className='flex items-center space-x-3 md:space-x-4 justify-end pr-5'>
+        <SpeakerXMarkIcon className='button' />
+        <input
+          className='w-14 md:w-28'
+          type='range'
+          value={volume}
+          onChange={(e) => setVolume(Number(e.target.value))}
+          min={0}
+          max={100}
+        />
+        <SpeakerWaveIcon className='button' />
       </div>
     </div>
   );
