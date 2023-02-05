45a3cf77059c31db6be69b9f07800e57aa9dc2d0
Calathea-Z <zsykes21@gmail.com>
2023-02-05 14:51:40 -0500

    Changed Search header title the melodius logo.

--- components/SearchTopTenByArtist.jsx
+++ components/SearchTopTenByArtist.jsx
@@ -1,3 +1,5 @@
+import MelodiousLogo from '../assets/Melodious-1.png'
+import Image from 'next/image'
 import useSpotify from "../hooks/useSpotify";
 import TopTenSongs from "./TopTenSongs";
 import CreatePlaylist from "../components/CreatePlaylist";
@@ -59,19 +61,17 @@ function SearchTopTenByArtist() {
       </header>
       <div className="`flex justify-between space-x-2 bg-gradient-to-b to-purp from-greeen h-80 text-white p-8`">
         <div className="flex">
-          <h1 className="pt-4 pl-1 pb-0 mb-0 w-9/12 flex flex-col space-y-10 text-yellow-400 font-semibold text-4xl tracking-wide leading-10">
-            Search
-          </h1>
+          <Image className='w-[7rem]' src= { MelodiousLogo } alt='Melodious Logo' />
           <CreatePlaylist />
         </div>
         <div className="flex flex-col">
-          <div className="flex pb-1 mt-20 items-center">
+          <div className="flex pb-1 mt-20 items-center justify-center">
             <form className="flex space-x-3" onChange={setInput}>
               <input
                 type="text"
                 name="songValues"
                 placeholder="...Radiohead"
-                className="text-xs text-blue-600 rounded-md w-[300px]"
+                className="text-xs text-blue-600 rounded-md w-[180px] md: w-[300px] "
               />
               <button
                 type="button"
@@ -79,7 +79,7 @@ function SearchTopTenByArtist() {
                 className="rounded-md border-greeen bg-purp p-3 border-solid border-2 opacity-75 cursor-pointer hover:scale-125 transition transform duration-100 ease-out font-bold text-xs  hover:bg-yellow-400 hover:text-white "
                 onClick={searchAndReturnResults}
               >
-                <h6>Search</h6>
+              Search
               </button>
             </form>
           </div>
